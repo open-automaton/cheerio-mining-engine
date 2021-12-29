9fdd0adfbb14efffad60ad7e51bf6e4b467fee33
Abbey Sparrow <abbey@khrome.net>
2021-12-29 16:26:58 -0700

    aligning modules

--- src/cheerio.js
+++ src/cheerio.js
@@ -1,9 +1,9 @@
-let AutomatonEngine = require('@open-automaton/automaton/src/automaton-engine');
+let AutomatonEngine = require('@open-automaton/automaton/src/automaton-engine.js');
 const Emitter = require('extended-emitter');
 
 const cheerio = require("cheerio");
 const request = require("postman-request");
-const DOM = require("@open-automaton/automaton/src/dom-tool").DOM;
+const DOM = require("@open-automaton/automaton/src/dom-tool.js").DOM;
 
 let Automaton = {};
 
